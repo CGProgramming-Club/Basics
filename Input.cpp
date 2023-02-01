0b5f4691356d467e9cd49f34741efa6b27215cb4
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-02-01 15:40:01 -0500

    Create Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // This creates a string variable.
+  string name;
+  
+  cout << "What's your name?" << endl;
+  cin >> name;
+  
+  cout << "Hello, " << name << endl;
+  
+  return 0;
+}
