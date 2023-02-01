77c6793d0b591704f23cde95c12a4a43d0a4c714
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-02-01 15:42:54 -0500

    Create Variables.cpp

---
+++ Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // String (contains text)
+  string greeting = "Hello!";
+  
+  // Integer (a whole number)
+  int number = 17;
+  
+  // Double (a decimal)
+  double pi = 3.14;
+  
+  // Boolean (can only be "true" or "false")
+  bool boolean = true;
+  
+  return 0;
+}
