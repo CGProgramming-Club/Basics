62d5dbf9677834e8e5b1c261ffd3d763e3801439
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-02-01 15:56:14 -0500

    Create While.cpp

---
+++ While.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Try changing the value to see how the loop behaves.
+  bool condition = true;
+  
+  while (condition) {
+    cout << "Hello, world!" << endl;
+  }
+  
+  return 0;
+}
