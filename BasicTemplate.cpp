f1a060286bd3001ad183c110cf7e8515d100f629
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-02-01 15:32:41 -0500

    Create BasicTemplate.cpp

---
+++ BasicTemplate.cpp
@@ -0,0 +1,9 @@
+// This is the way most if not all of your projects will start. Feel free to copy this as a template if you don't know how to start.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  return 0;
+}
