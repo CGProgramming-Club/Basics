58b4db9e4516a1e53eec9943bc035560fcd7dd4a
Selisine <Aidenpike113@gmail.com>
2023-02-08 15:35:55 -0500

    Update shopkeepExV1.cpp

--- miniprojects/shopkeep/shopkeepExV1.cpp
+++ miniprojects/shopkeep/shopkeepExV1.cpp
@@ -21,24 +21,37 @@ int main() {
         cout << "1. Bread" << endl;
         cout << "2. Sword of Stone" << endl;
         cout << "3. Crown of the Forest" << endl;
-        cout << "4. I just wanna buy the item!" << endl;
+        cout << "4. Life of Force Staff" << endl;
+        cout << "5. Night Poison" << endl;
+        cout << "6. Potion Pouch" << endl;
+        cout << "7. I just wanna buy the item!" << endl;
         cin >> examineChoice;
-
         
         if (examineChoice == 1){
-            cout << "Crafted in the finest mills and cooked with the most stable of ovens, this bread is the most legendary block of grain known to mankind. [$600]\n";
+            cout << "Crafted in the finest mills and cooked with the most stable of ovens, this bread is the most legendary block of grain known to mankind. [5 Rubies]\n";
         }
         else if (examineChoice == 2){
-            cout << "A extremely polished, hard rock on a pommel fit for a king's king. It glistens in the light like a diamond in the rough. [$1500]\n";
+            cout << "A extremely polished, hard rock on a pommel fit for a king's king. It glistens in the light like a diamond in the rough. [1,500,000 Rubies]\n";
         }
         else if (examineChoice == 3){
-            cout << "Found deep within the heart of the forest and forged by a master of the druidic arts. It has a soft glow, greener than a polished jade. [$8000]\n";
+            cout << "Found deep within the heart of the forest and forged by a master of the druidic arts. It has a soft glow, greener than a polished jade. [600 Rubies]\n";
         }
         else if (examineChoice == 4){
+            cout << "A wooden staff with a red stone at the top, covered in roses. [1,200,000 rubies]\n";
+        }
+        else if (examineChoice == 5){
+            cout << "A lavender glass bottle filled with a deadly purple substance. [200 rubies]\n";
+        }
+        else if (examineChoice == 6)
+            cout << "A brown satchel filled with needed survival potions. [50 rubies]\n";
+        else if (examineChoice == 7){
             cout << "Which item would you like to buy?\n";
             cout << "1. Bread" << endl;
             cout << "2. Sword of Stone" << endl;
-            cout << "3. Crown of the Forest" << endl;
+            cout << "3. Leaf Keeper" << endl;
+            cout << "4. Life of Force Staff" << endl;
+            cout << "5. Night Potion" << endl;
+            cout << "6. Potion Pouch" << endl;
             cin >> itemChoice;
 
             if (itemChoice == 1){
@@ -59,10 +72,10 @@ int main() {
             }
             if (itemChoice == 3){
                 if (moneyAmt >= 8000){
-                    cout << "Great choice. Here is your ancient Crown of the Forest. Eònan pays his respects." << endl;
+                    cout << "Great choice. Here is your ancient Leaf Keeper. Eònan pays his respects." << endl;
                 }
                 else if (moneyAmt < 8000) {
-                    cout << "You are too poor to buy the ancient Crown of the Forest. Beware of the forest for the next few days, for it may be against you." << endl;
+                    cout << "You are too poor to buy the ancient Leaf Keeper. Beware of the forest for the next few days, for it may be against you." << endl;
                 }
             }
         }
